7b29991a74daa5d05ed46a19593347839d5f4cf3
DOWGLAS <dowglasmaia@live.com>
2020-10-14 15:56:22 -0300

    Configurando Projeto

---
+++ src/main/java/org/maia/admin/AdminApisApplication.java
@@ -0,0 +1,13 @@
+package org.maia.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminApisApplication.class, args);
+	}
+
+}
